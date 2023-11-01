0e85c66466e5cf071b2fa49807e9c914d77f429b
Samet Aydinhan <samet.aydinhan879@gmail.com>
2023-11-01 21:46:40 +0300

    feat:keystroke validations

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -8,6 +8,9 @@ export default function Login() {
     password: "",
   });
 
+  const emailIsInvalid =
+    enteredValues.email !== "" && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
     setEnteredValues({
@@ -46,10 +49,13 @@ export default function Login() {
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className='control-error'>
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className='control no-margin'>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor='password'>Password</label>s
           <input
             id='password'
             type='password'
